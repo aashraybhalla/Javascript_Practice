13dcebc8e4af6eee509f7480793e7ffeffccc39f
AASHRAY BHALLA <aashray21csu553@ncuindia.edu>
2023-07-19 00:40:36 +0530

    Generating Outputs on Console

---
+++ main.js
@@ -0,0 +1,27 @@
+console.log('Hello World!');
+
+
+const person = {
+
+    name : {
+    firstName : 'Aashray',
+    lastName : 'Bhalla'
+    },
+
+    age : 20 ,
+
+    Address : {
+        houseNumber : '5B/33',
+        street : 'Tilak Nagar',
+        city : 'Delhi'
+
+    },
+
+    hobbies : ['Movies', 'Development', 'Travelling']
+
+
+}
+console.log(person.name);
+console.log(person.age);
+console.log(person.Address);
+console.log(person.hobbies[1]);
