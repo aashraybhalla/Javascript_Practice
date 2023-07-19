a4f822c2a345efc9d467f5e0e63f834857e2c260
AASHRAY BHALLA <aashray21csu553@ncuindia.edu>
2023-07-19 09:05:11 +0530

    Functions, Oops, Constructor, Instantiate Object

--- main.js
+++ main.js
@@ -128,3 +128,37 @@ switch(color) {
 
 }
 
+
+// Functions
+
+function addNums(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(addNums(5,7));
+
+
+// Oops
+
+//Constructor Function
+function Person(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = new Date(dob);
+}
+
+Person.prototype.getBirthYear = function() {
+    return this.dob.getFullYear();
+}
+
+Person.prototype.getFullName = function() {
+    return `${this.firstName} ${this.lastName}`;
+}
+
+//Instantiate Object
+const person1 = new Person('John', 'Doe', '12-3-2003');
+const person2 = new Person('Willian', 'Djovik', '12-8-2001');
+
+console.log(person2);
+console.log(person1.getFullName());
+
